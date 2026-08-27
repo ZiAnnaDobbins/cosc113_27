421e9797a6bc5c40adc84d4245fbb958de9ced18
ZiAnnaDobbins <ziannadobbins@gmail.com>
2026-08-27 10:28:01 -0400

    4 student attuributs added

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main()
+
+
+    String Fn;
+    String Ln;
+    int id;
+    double GPA;
+
+
+    public static <string> void main(string[] args){
+        System system;
+        system.out.println("hello java");
+
+
+}
